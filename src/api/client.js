aa4de79fcf869c698e672f7cb6e7d06b5bf368c4
zeussincript <ZEUS@sincript.com>
2026-09-15 01:23:51 -0700

    prefix route issue fixed

--- src/api/client.js
+++ src/api/client.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL
+const API_URL = import.meta.env.VITE_API_URL?.replace(/\/$/, '')
 
 if (!API_URL) throw new Error('VITE_API_URL is not configured')
 
--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-
+  <BrowserRouter basename={import.meta.env.BASE_URL.replace(/\/$/, '')}>
+    <App />
+  </BrowserRouter>
 )
